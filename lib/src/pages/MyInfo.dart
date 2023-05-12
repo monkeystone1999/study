3b45c1f543dac0427e4c5530ae99634a9ca52959
DESKTOP-BQT3FEG\user <dbsejrdid123@gmail.com>
2023-05-12 10:39:47 +0900

    2023-05-12

---
+++ lib/src/pages/MyInfo.dart
@@ -0,0 +1,17 @@
+import 'dart:js_interop';
+
+import 'package:flutter/material.dart';
+import 'package:production/src/pages/presentation/input_1.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
